53a60f20330aad39246d658feaae4d1a5c99ed90
SUJALSINGH007 <singhsujal52938@gmail.com>
2025-09-29 19:14:46 +0530

    completd the Cart Drawer with content and check out

--- frontend/src/Components/Layout/CartDrawer.jsx
+++ frontend/src/Components/Layout/CartDrawer.jsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import CartContents from "../Cart/CartContents";
 
 const CartDrawer = ({drawerOpen, CartDawerToggle}) => {
 
   return(
-    <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
+    <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
       {/* close buttion */}
       <div className="flex justify-end p-4 ">
         <button onClick={CartDawerToggle}>
-          <IoMdClose className="h-5 w-5 text-gray-600"/>
+          <IoMdClose className="h-6 w-6 text-gray-600 hover:cursor-pointer"/>
         </button>
       </div>
+
+        {/* cart contents with scrolleable area */}
+      <div className="flex-grow p-4 overflow-y-auto  ">
+        <h2 className="text-xl font-semibold text-center">Your Cart</h2>
+        {/* Components for cart contaents */}
+        <CartContents />
+      </div>
+
+      {/* Checkout buttion fixed at the bottom */}
+      <div className="p-4 bg-white sticky bottom-0">
+        <button className="w-full bg-black text-white py-3 rounded-lg hover:bg-gray-200 hover:text-black transition">Checkout</button>
+        <p className="text-xs tracking-tighter text-gray-500 text-center mt-2">
+          Shipping, taxes, and discount codes calculated at checkout.</p>
+      </div>
+
     </div>
   )
 }
