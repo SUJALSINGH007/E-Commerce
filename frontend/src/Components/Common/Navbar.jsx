558cb46c052af1a6c07bb0dae9ea723b95e50690
SUJALSINGH007 <singhsujal52938@gmail.com>
2025-09-29 19:15:37 +0530

    added navbar for mobile as well

--- frontend/src/Components/Common/Navbar.jsx
+++ frontend/src/Components/Common/Navbar.jsx
@@ -3,10 +3,16 @@ import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight } from "react-i
 import SearchBar from "./SearchBar";
 import CartDrawer from "../Layout/CartDrawer";
 import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () =>{
 
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+
+  const toogleNavDrawer = () =>{
+    setNavDrawerOpen(!navDrawerOpen);
+  }
 
   const CartDawerToggle = () =>{
     setDrawerOpen(!drawerOpen);
@@ -38,7 +44,7 @@ return(
       </div>
 
       <div className="flex items-center space-x-4">
-        <Link to="/profile" className="hover:text-black">
+        <Link to="/" className="hover:text-black">
         <HiOutlineUser className="h-5 w-5 text-gray-700" />
         </Link>
 
@@ -48,17 +54,46 @@ return(
         </button>
 
         {/* {search} */}
-        <div className="overflow-hidden">
+        <div className="overflow-hidden ">
           <SearchBar />
         </div>
 
-        <button className="md:hidden ">
+        <button onClick={toogleNavDrawer} className="md:hidden ">
         <HiBars3BottomRight className="h-6 w-6 text-gray-700 hover:cursor-pointer" />
         </button>
       </div>
     </nav>
 
     <CartDrawer drawerOpen={drawerOpen} CartDawerToggle={CartDawerToggle} />
+
+    {/* Mobile Navigation */}
+    <div className={`fixed h-full top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3  bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0":"-translate-x-full"}`}>
+      <div  className="flex justify-end p-4">
+        <button onClick={toogleNavDrawer}>
+          <IoMdClose className="h-6 w-6 text-gray-600"/>
+        </button>
+      </div>
+      <div className="p-4">
+        <h2 className="text-xl font-semibold mb-4 text-center">Menu</h2>
+        <nav className="space-y-4">
+            <Link to="#"
+            onClick={toogleNavDrawer}
+            className="block text-gray-600 hover:text-black  uppercase ">T-shirt</Link>
+
+            <Link to="#"
+            onClick={toogleNavDrawer}
+            className="block text-gray-600 hover:text-black uppercase">pants</Link>
+
+            <Link to="#"
+            onClick={toogleNavDrawer}
+            className="block text-gray-600 hover:text-black uppercase">kit</Link>
+
+            <Link to="#"
+            onClick={toogleNavDrawer}
+            className="block text-gray-600 hover:text-black uppercase">originals</Link>
+        </nav>
+      </div>
+    </div>
     </>
   )
 }
