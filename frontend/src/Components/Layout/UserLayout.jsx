fba17d4ec8941598b05a770a6577342dfab99abf
SUJALSINGH007 <singhsujal52938@gmail.com>
2025-09-30 20:36:25 +0530

    added the footer in user layout

--- frontend/src/Components/Layout/UserLayout.jsx
+++ frontend/src/Components/Layout/UserLayout.jsx
@@ -1,9 +1,14 @@
+import Footer from "../Common/Footer";
 import Header from "../Common/Header";
 
 const UserLayout = () => {
   return(
     <div>
+      {/* header */}
       <Header />
+      {/* main content */}
+      {/* footer */}
+      <Footer />
     </div>
   )
 }
