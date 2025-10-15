239e123afb97a80107a777a0aa54bce3eee4bc01
SUJALSINGH007 <singhsujal52938@gmail.com>
2025-10-15 20:30:11 +0530

    Added two more sections - 65% Home page is completed

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import Hero from "../Components/Layout/Hero"
+import GenderCollectionSection from "../Components/Products/GenderCollectionSection";
+import NewArrivels from "../Components/Products/NewArrivels";
 
 const Home = () =>{
   return(
     <div>
     <Hero />
+    <GenderCollectionSection />
+    <NewArrivels />
     </div>
   )
 }
