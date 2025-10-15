4231ae9f69d477ebe6395a58667f95708ea554b8
SUJALSINGH007 <singhsujal52938@gmail.com>
2025-10-15 20:28:43 +0530

    added genter selction section on Home pagee

---
+++ frontend/src/Components/Products/GenderCollectionSection.jsx
@@ -0,0 +1,33 @@
+import MensCollectionImage from "../../assets/mens-Collection.webp";
+import WomensCollectionImage from "../../assets/womens-Collection.webp"
+import { Link } from "react-router-dom";
+
+const GenderCollectionSection = () => {
+  return (
+    <section className="py-16 px-4 lg:px-10 ">
+      <div className="container mx-auto flex flex-col md:flex-row gap-7">
+        <div className="relative flex-1 ">
+          <img src={WomensCollectionImage}
+          alt="women's collection"
+          className="w-full h-[700px] object-cover"/>
+          <div className="absolute bottom-8 left-8 bg-white/90 p-4">
+          <h2 className="text-2xl font-bold text-gray-900 mb-3">Womens's Collection</h2>
+          <Link to="/collections/all?gender=Women" className="text-gray-900 underline">Shop Now</Link>
+          </div>
+        </div>
+        <div className="relative flex-1">
+          <img src={MensCollectionImage}
+          alt="Men's collection"
+          className="w-full h-[700px] object-cover"/>
+          <div className="absolute bottom-8 left-8 bg-white/90 p-4">
+          <h2 className="text-2xl font-bold text-gray-900 mb-3">Mens's Collection</h2>
+          <Link to="/collections/all?gender=Men" className="text-gray-900 underline">Shop Now</Link>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  )
+};
+
+export default GenderCollectionSection;
